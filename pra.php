18e0465702d1c56003783c6ae0a7036136efc3cf
wei <lin951016@gmail.com>
2023-10-06 16:15:56 +0800

    add pra

---
+++ pra.php
@@ -0,0 +1,20 @@
+<h2>閏年</h2>
+<?php
+$year = 2000;
+echo "年份:" . $year;
+echo "<hr>";
+
+if ($year % 4 == 0) {
+    if ($year % 100 != 0) {
+        echo $year . "是閏年";
+    }else{
+        echo $year."是平年"
+    }
+} else {
+    if($year%400== ){
+        
+    }
+    echo $year . "是平年";
+}
+
+?>
