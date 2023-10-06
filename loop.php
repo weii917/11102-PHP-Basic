ccf04f853a11e926d5b1981fe88047c04a9e9eb4
wei <lin951016@gmail.com>
2023-10-06 09:31:42 +0800

    add myloop

--- loop.php
+++ loop.php
@@ -1,7 +1,13 @@
 <?php
 $sum=0;
-for($i=1;$i<=100;$i=$i+1){
-    $sum=$sum+$i;
+for($i=1;$i<=10;$i=$i+1){
+    // $sum=$sum+$i;
+    echo '當$i='.$i.'時';
+    echo '$sum+$i的結果是';
+    echo $sum . '+' . $i. '=';
+    echo $sum+$i;
+    echo"<br>";
+    $sum+=$i;
 }
 
 echo "1加到100為".$sum;
