3214350517c2a2aeaafba565c15081e6409e3325
wei <lin951016@gmail.com>
2023-10-13 16:24:43 +0800

    add

--- pra.php
+++ pra.php
@@ -8,7 +8,7 @@ if ($year % 4 == 0) {
     if ($year % 100 != 0) {
         echo $year . "是閏年";
     }else{
-        echo $year."是平年"
+        echo $year."是平年";
     }
 } else {
     if($year%400==0){
